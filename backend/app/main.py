15bd4727012fa6d8a96ab99dd26fe8a339ae2ff2
swaekaa <sawariaekaansh@gmail.com>
2026-05-07 00:14:56 +0530

    fix: updated CORS middleware configuration

--- backend/app/main.py
+++ backend/app/main.py
@@ -38,7 +38,7 @@ app.include_router(files_router)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["*"],  # temporary (for testing)
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
